77446e7f89b5b30a92e9e68adec478311e58bb6f
VLadimir_Ostroverkh <vovaostroverh@mail.ru>
2025-05-28 01:32:36 +0300

    обновлен файл

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-from flask import Flask, render_template, request, render_template_string
+from flask import Flask, render_template, request
 import pickle
-import sklearn
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import AdaBoostRegressor
-from sklearn.preprocessing import MinMaxScaler
-import numpy as np
-import pandas as pd
 
 # Создание экземпляра Flask
 app = Flask(__name__)
